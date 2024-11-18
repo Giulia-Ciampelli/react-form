a20316b45abf7963a795655067944a073c7fb7cd
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-11-18 15:14:04 +0100

    docs: add post function

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,13 +9,36 @@ const initialPosts = [
 
 export default function Main() {
     const [posts, setPosts] = useState(initialPosts);
+    const [newPost, setNewPost] = useState('');
+
+    // funzione per aggiungere un post nuovo
+    function addPost(e) {
+        e.preventDefault();
+
+        // clonazione array
+        const newPosts = [
+            newPost,
+            ...posts
+        ];
+
+        // aggiornamento UI
+        setPosts(newPosts);
+        setNewPost('');
+    }
 
     return (
         <main>
-            <form>
-
+            <form onSubmit={addPost}>
+                <div>
+                    <input type="text" id="textInput" placeholder="Titolo nuovo post" value={newPost} onChange={e => setNewPost(e.target.value)} />
+                    <button type="submit">
+                        Aggiungi post
+                    </button>
+                </div>
                 <ul>
-                    {posts.map((post, index) => <li key={index}>{post}</li>)}
+                    {posts.map((post, index) => <li key={index}>
+                        {post}
+                    </li>)}
                 </ul>
             </form>
         </main>
